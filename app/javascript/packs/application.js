9684c768ab4f45f0902885aa327ebc8d4f70687c
toshiki-hato <66351093+toshiki-hato@users.noreply.github.com>
2020-06-19 15:00:24 +0900

    完成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("jquery")
 require("../message")
+require("../users")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
