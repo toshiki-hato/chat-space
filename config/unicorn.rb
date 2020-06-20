35ffe2625391de001cc35fdb30682e7314bacd18
toshiki-hato <66351093+toshiki-hato@users.noreply.github.com>
2020-06-20 21:18:59 +0900

    修正５

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -6,7 +6,7 @@ worker_processes 1
 working_directory "#{app_path}/current"
 
 # それぞれ、sharedの中を参照するよう変更
-listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+listen "#{app_path}/tmp/sockets/unicorn.sock"
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
